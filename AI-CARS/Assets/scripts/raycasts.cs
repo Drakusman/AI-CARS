af843c1bd95e76f532524731de6fcdfe0a4b2722
Drakusman <70572092+Drakusman@users.noreply.github.com>
2020-10-15 12:12:26 +0200

    Update raycasts.cs
    colision detection,
    Debug show info etc..

--- AI-CARS/Assets/scripts/raycasts.cs
+++ AI-CARS/Assets/scripts/raycasts.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class raycasts : MonoBehaviour
 {
+    //Debug - all date that are showed to the dev by consol or graphical
+
     //switch between visibility of rays 
-    public bool Debug_show_rays = true;
+    public bool DebugShowRaysToggle = true;
+    public bool DebugShowHitInfoToggle = true;
+    //Debug range of raycast
+    public float DebugRayLength = 1f;
+    //range of raycast
+    public float rayLength = 5f;
     // all 8 raycasts and hits, that check collisions.
-    Ray F, FR, R, RB, B, BL, L, FL;
-    RaycastHit H_F, H_FR, H_R, H_RB, H_B, H_BL, H_L, H_FL;
+    /* 
+     * F - Front
+     * FR - FrontRight
+     * BR - BackRight
+     * B - Back
+     * BL - BackLeft
+     * L - Left
+     * FL - FrontLeft
+    */ 
+    Ray F, FR, R, BR, B, BL, L, FL;
+    RaycastHit H_F, H_FR, H_R, H_BR, H_B, H_BL, H_L, H_FL;
 
     void Start()
     {
@@ -18,29 +34,49 @@ public class raycasts : MonoBehaviour
     void Update()
     {
         showRaycast();
+        startRayCasting();
     }
     //display debug rays of raycasts
     private void showRaycast()
     {
-        if(Debug_show_rays)
+        if(DebugShowRaysToggle)
         {
             //front
-            Debug.DrawRay(transform.position, transform.forward, Color.green);
+            Debug.DrawRay(transform.position, transform.forward * DebugRayLength, Color.green);
             //back
-            Debug.DrawRay(transform.position, -transform.forward, Color.green);
+            Debug.DrawRay(transform.position, -transform.forward * DebugRayLength, Color.green);
             //right
-            Debug.DrawRay(transform.position, transform.right, Color.green);
+            Debug.DrawRay(transform.position, transform.right * DebugRayLength, Color.green);
             //left
-            Debug.DrawRay(transform.position, -transform.right, Color.green);
+            Debug.DrawRay(transform.position, -transform.right * DebugRayLength, Color.green);
             //front right
-            Debug.DrawRay(transform.position, transform.forward + transform.right, Color.green);
+            Debug.DrawRay(transform.position, (transform.forward + transform.right) * DebugRayLength, Color.green);
             // front left
-            Debug.DrawRay(transform.position, transform.forward - transform.right, Color.green);
+            Debug.DrawRay(transform.position, (transform.forward - transform.right) * DebugRayLength, Color.green);
             //back right
-            Debug.DrawRay(transform.position, -transform.forward + transform.right, Color.green);
+            Debug.DrawRay(transform.position, (-transform.forward + transform.right) * DebugRayLength, Color.green);
             //back left
-            Debug.DrawRay(transform.position, -transform.forward - transform.right, Color.green);
+            Debug.DrawRay(transform.position, (-transform.forward - transform.right) * DebugRayLength, Color.green);
         }
         
+    }
+    private void startRayCasting()
+    {
+        if (Physics.Raycast(F, out H_F, rayLength))
+        {
+            print(DebugShowHitInfo(H_F) + "Front");
+        }    
+       
+        Physics.Raycast(FR, out H_FR, rayLength);
+        Physics.Raycast(FL, out H_FL, rayLength);
+        Physics.Raycast(B, out H_B, rayLength);
+        Physics.Raycast(BL, out H_BL, rayLength);
+        Physics.Raycast(BR, out H_BR, rayLength);
+        Physics.Raycast(L, out H_L, rayLength);
+        Physics.Raycast(R, out H_R, rayLength);
+    }
+    private string DebugShowHitInfo(RaycastHit hit)
+    {
+        return "Detected raycast hit name: " + hit.transform.name + ". At distance: " + hit.distance +". Direction: ";
     }
 }
