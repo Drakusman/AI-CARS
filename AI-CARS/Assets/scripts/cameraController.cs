975a0c4bfaaf11820d1e81f7db2dee4d0cdde8af
Drakusman <drakusman@gmail.com>
2021-01-14 16:23:08 +0100

    Update 0.1
    Added camera mechanic,
    Added road test1,road test2, road test3.
    Added new option to car.cs (traction type, max speed, mph or km/h)
    Adjustment of wheel colliders

---
+++ AI-CARS/Assets/scripts/cameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform car;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+    public float zoomRatio;
+    public float defaultFOV; // Field of View
+
+    private float rotation_vector;
+    private void FixedUpdate()
+    {
+        Vector3 local_velocity = car.InverseTransformDirection(car.GetComponent<Rigidbody>().velocity);
+        if (local_velocity.z < -0.5f)
+        {
+            rotation_vector = car.eulerAngles.y + 100;
+        }
+        else
+        {
+            rotation_vector = car.eulerAngles.y;
+        }
+
+        float acceleration = car.GetComponent<Rigidbody>().velocity.magnitude;
+        Camera.main.fieldOfView = defaultFOV + acceleration * zoomRatio * Time.deltaTime;
+
+    }
+    private void LateUpdate()
+    {
+        float wantedAngle = rotation_vector;
+        float wantedHeight = car.position.y + height;
+        float myAngle = transform.eulerAngles.y;
+        float myHeight = transform.position.y;
+
+        myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+        myHeight = Mathf.LerpAngle(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, myAngle, 0);
+
+        transform.position = car.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        Vector3 temp = transform.position;
+        temp.y = myHeight;
+        transform.position = temp;
+
+        transform.LookAt(car);
+
+        }
+}
--- AI-CARS/Assets/scripts/car.cs
+++ AI-CARS/Assets/scripts/car.cs
@@ -4,46 +4,109 @@ using UnityEngine;
 
 public class car : MonoBehaviour
 {
+    public enum Traction
+    {
+        front, back, _4x4
+    }
+    public enum SpeedValue
+    {
+        km_h,mph
+    }
+   
     //car wheel colliders
+    [Header("Wheel Colliders")]
     public WheelCollider wheelColliderFL;
     public WheelCollider wheelColliderFR;
     public WheelCollider wheelColliderBL;
     public WheelCollider wheelColliderBR;
 
     // car wheel transforms
+    [Header("Wheel Transforms")]
     public Transform wheelFL;
     public Transform wheelFR;
     public Transform wheelBL;
     public Transform wheelBR;
 
+    // is car braking or not.
+    private bool braking = false;
+
     private Rigidbody rigidbody;
-    //speed
+
+    //acceleration
+    [Header("Motor settings")]
     public float motorTorque = 400f;
-    //max steer of wheels
+    public float maxSpeed = 100f;
+    public SpeedValue speedValue;
+    //max angle of steering
+    [Header("Wheel settings")]
+    public Traction traction;
     public float maxSteer = 20f;
 
-    private void Start()
+    private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
-        
-    }
-    private void FixedUpdate()
-    {
-        
+
+        maxSpeed /= 3.6f;
+   
     }
     private void Update()
     {
-        wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        if(speedValue == SpeedValue.km_h)
+        {
+            print("Velocity: " + Mathf.Round(rigidbody.velocity.magnitude * 3.6f) + " Km/h");
+        }
+        else
+        {
+            print("Velocity: " + Mathf.Round(rigidbody.velocity.magnitude * 3.6f * 0.62f) + " Mph");
+        }
+       
+    }
 
+    private void FixedUpdate()
+    {
+        
+        //car's traction type
+        switch(traction)
+        {
+            case Traction.front:
+            {
+                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    break;
+            }
+            case Traction.back:
+                {
+                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    break;
+                }
+            case Traction._4x4:
+                {
+                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    break;
+                }
+        }
+        
+
+        //max steering angle of wheels
         wheelColliderFL.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
         wheelColliderFR.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
 
+        //wheel rotation
         front_wheel_rotation();
+
+        //check if you pass the max speed
+        var velocityVector = rigidbody.velocity;
+
+        if (rigidbody.velocity.magnitude > maxSpeed)
+        {
+            rigidbody.velocity = velocityVector.normalized * maxSpeed;
+        }
     }
-    //methode which set rotation and position of front wheels while driving
+    //methode which set rotation and position of front wheels while driving *just animation of rotation*
     private void front_wheel_rotation()
     {
         var pos = Vector3.zero;
