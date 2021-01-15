37a3b1917515b25be935d6264f4339325a972e88
Drakusman <drakusman@gmail.com>
2021-01-15 12:30:24 +0100

    adjustment
    adjustment car mechanic

--- AI-CARS/Assets/scripts/car.cs
+++ AI-CARS/Assets/scripts/car.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class car : MonoBehaviour
 {
+    public enum Gearbox
+    {
+        manual, automatic
+    }
     public enum Traction
     {
         front, back, _4x4
@@ -27,38 +31,82 @@ public class car : MonoBehaviour
     public Transform wheelBL;
     public Transform wheelBR;
 
-    // is car braking or not.
-    private bool braking = false;
-
-    private Rigidbody rigidbody;
+    private Rigidbody _rigidbody;
 
     //acceleration
     [Header("Motor settings")]
     public float motorTorque = 400f;
     public float maxSpeed = 100f;
-    public SpeedValue speedValue;
+    
     //max angle of steering
     [Header("Wheel settings")]
     public Traction traction;
     public float maxSteer = 20f;
+    public float brakeTorque = 10000f;
+    public bool handBrake = false;
 
+    [Header("Cockpit settings")]
+    public bool DebugShowSpeed = true;
+    public SpeedValue speedValue;
+    public float speed;
+    public float torque;
+
+    [Header("Transmission settings")]
+    public Gearbox gearbox;
+    public int gears = 5;
+    public int current_gear = 1;
+
+    private string keyPressed;
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
 
         maxSpeed /= 3.6f;
    
     }
     private void Update()
     {
-        if(speedValue == SpeedValue.km_h)
+        //update the current torque
+        if(traction==Traction.back || traction==Traction._4x4)
         {
-            print("Velocity: " + Mathf.Round(rigidbody.velocity.magnitude * 3.6f) + " Km/h");
+            torque = wheelColliderBL.motorTorque;
         }
         else
         {
-            print("Velocity: " + Mathf.Round(rigidbody.velocity.magnitude * 3.6f * 0.62f) + " Mph");
+            torque = wheelColliderFL.motorTorque;
         }
+        
+        if(speedValue == SpeedValue.km_h)
+        {
+            speed = Mathf.Round(_rigidbody.velocity.magnitude * 3.6f);
+        }
+        else
+        {
+            speed = Mathf.Round(_rigidbody.velocity.magnitude * 3.6f * 0.62f);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            keyPressed = "w";
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            keyPressed = "s";
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            keyPressed = " ";
+        }
+      
+
+        if (speedValue == SpeedValue.km_h && DebugShowSpeed)
+        {
+            print("Velocity: " + Mathf.Round(_rigidbody.velocity.magnitude * 3.6f) + " Km/h");
+        }
+        if(speedValue == SpeedValue.mph && DebugShowSpeed)
+        {
+            print("Velocity: " + Mathf.Round(_rigidbody.velocity.magnitude * 3.6f * 0.62f) + " Mph");
+        }
+
        
     }
 
@@ -66,26 +114,173 @@ public class car : MonoBehaviour
     {
         
         //car's traction type
-        switch(traction)
+        switch (traction)
         {
             case Traction.front:
             {
-                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    
+                        switch (keyPressed)
+                        {
+                            case "w":
+                                {
+                                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque * 2;
+                                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque * 2;
+
+                                    break;
+                                }
+
+                            case "s":
+                                {  
+                                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+
+                                    break;
+                                }
+
+                            case " ":
+                                {
+                                    if (handBrake == false)
+                                    {
+                                        wheelColliderBL.motorTorque = 0;
+                                        wheelColliderBR.motorTorque = 0;
+                                        wheelColliderFL.motorTorque = 0;
+                                        wheelColliderFR.motorTorque = 0;
+
+                                        wheelColliderBL.brakeTorque = brakeTorque;
+                                        wheelColliderBR.brakeTorque = brakeTorque;
+                                        wheelColliderFL.brakeTorque = brakeTorque;
+                                        wheelColliderFR.brakeTorque = brakeTorque;
+
+                                        handBrake = true;
+                                    }
+                                    else
+                                    {
+                                        wheelColliderBL.brakeTorque = 0;
+                                        wheelColliderBR.brakeTorque = 0;
+                                        wheelColliderFL.brakeTorque = 0;
+                                        wheelColliderFR.brakeTorque = 0;
+                                        handBrake = false;
+                                    }
+                                keyPressed = "";
+                                break;
+                                }
+                        }
+                    
+
                     break;
             }
             case Traction.back:
                 {
-                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                   
+                        switch (keyPressed)
+                        {
+                            case "w":
+                                {
+                                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque * 2;
+                                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque * 2;
+
+                                    break;
+                                }
+
+                            case "s":
+                                { 
+                                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+
+                                    break;
+                                }
+
+                            case " ":
+                                {
+                                    if (!handBrake)
+                                    {
+                                        wheelColliderBL.motorTorque = 0;
+                                        wheelColliderBR.motorTorque = 0;
+                                        wheelColliderFL.motorTorque = 0;
+                                        wheelColliderFR.motorTorque = 0;
+
+                                        wheelColliderBL.brakeTorque = brakeTorque;
+                                        wheelColliderBR.brakeTorque = brakeTorque;
+                                        wheelColliderFL.brakeTorque = brakeTorque;
+                                        wheelColliderFR.brakeTorque = brakeTorque;
+
+                                        handBrake = true;
+                                    }
+                                    else
+                                    {
+                                        wheelColliderBL.brakeTorque = 0;
+                                        wheelColliderBR.brakeTorque = 0;
+                                        wheelColliderFL.brakeTorque = 0;
+                                        wheelColliderFR.brakeTorque = 0;
+                                        handBrake = false;
+                                    }
+                                keyPressed = "";
+                                break;
+                                }
+                        }
+                    
+
                     break;
                 }
             case Traction._4x4:
                 {
-                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                    
+                        switch (keyPressed)
+                        {
+                            case "w":
+                                {
+                                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+                                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+
+                                    break;
+                                }
+
+                            case "s":
+                                {
+                                    wheelColliderBL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderBR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFL.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+                                    wheelColliderFR.motorTorque = Input.GetAxis("Vertical") * brakeTorque;
+
+                                    break;
+                                }
+
+                            case " ":
+                                {
+                                    if (handBrake == false)
+                                    {
+                                        wheelColliderBL.motorTorque = 0;
+                                        wheelColliderBR.motorTorque = 0;
+                                        wheelColliderFL.motorTorque = 0;
+                                        wheelColliderFR.motorTorque = 0;
+
+                                        wheelColliderBL.brakeTorque = brakeTorque;
+                                        wheelColliderBR.brakeTorque = brakeTorque;
+                                        wheelColliderFL.brakeTorque = brakeTorque;
+                                        wheelColliderFR.brakeTorque = brakeTorque;
+
+                                        handBrake = true;
+                                    }
+                                    else
+                                    {
+                                        wheelColliderBL.brakeTorque = 0;
+                                        wheelColliderBR.brakeTorque = 0;
+                                        wheelColliderFL.brakeTorque = 0;
+                                        wheelColliderFR.brakeTorque = 0;
+                                        handBrake = false;
+                                    }
+                                keyPressed = "";
+                                break;
+                                }
+                        }  
+                    
+
                     break;
                 }
         }
@@ -99,12 +294,13 @@ public class car : MonoBehaviour
         front_wheel_rotation();
 
         //check if you pass the max speed
-        var velocityVector = rigidbody.velocity;
+        var velocityVector = _rigidbody.velocity;
 
-        if (rigidbody.velocity.magnitude > maxSpeed)
+        if (_rigidbody.velocity.magnitude > maxSpeed)
         {
-            rigidbody.velocity = velocityVector.normalized * maxSpeed;
+            _rigidbody.velocity = velocityVector.normalized * maxSpeed;
         }
+        
     }
     //methode which set rotation and position of front wheels while driving *just animation of rotation*
     private void front_wheel_rotation()
