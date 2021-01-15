921eec664863afd4ce7e3a30a6451e9178ebb0de
Drakusman <drakusman@gmail.com>
2021-01-15 20:13:16 +0100

    Pathfinding and simply AI Cars
    added pathfiding, auto generated paths for ai cars that are able to follow.

---
+++ AI-CARS/Assets/scripts/CarEngine.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarEngine : MonoBehaviour {
+
+    public Transform path;
+    public float maxSteerAngle = 45f;
+    public float turnSpeed = 5f;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public WheelCollider wheelRL;
+    public WheelCollider wheelRR;
+    public float maxMotorTorque = 80f;
+    public float maxBrakeTorque = 150f;
+    public float currentSpeed;
+    public float maxSpeed = 100f;
+    public Vector3 centerOfMass;
+    public bool isBraking = false;
+    public Texture2D textureNormal;
+    public Texture2D textureBraking;
+    public Renderer carRenderer;
+
+    [Header("Sensors")]
+    public float sensorLength = 3f;
+    public Vector3 frontSensorPosition = new Vector3(0f, 0.2f, 0.5f);
+    public float frontSideSensorPosition = 0.2f;
+    public float frontSensorAngle = 30f;
+
+    private List<Transform> nodes;
+    private int currectNode = 0;
+    private bool avoiding = false;
+    private float targetSteerAngle = 0;
+
+    private void Start() {
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++) {
+            if (pathTransforms[i] != path.transform) {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        Sensors();
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+        Braking();
+        LerpToSteerAngle();
+    }
+
+    private void Sensors() {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        sensorStartPos += transform.forward * frontSensorPosition.z;
+        sensorStartPos += transform.up * frontSensorPosition.y;
+        float avoidMultiplier = 0;
+        avoiding = false;
+
+        //front right sensor
+        sensorStartPos += transform.right * frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)) {
+            if (!hit.collider.CompareTag("Terrain")) {
+                Debug.DrawLine(sensorStartPos, hit.point);
+                avoiding = true;
+                avoidMultiplier -= 1f;
+            }
+        }
+
+        //front right angle sensor
+        else if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)) {
+            if (!hit.collider.CompareTag("Terrain")) {
+                Debug.DrawLine(sensorStartPos, hit.point);
+                avoiding = true;
+                avoidMultiplier -= 0.5f;
+            }
+        }
+
+        //front left sensor
+        sensorStartPos -= transform.right * frontSideSensorPosition * 2;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)) {
+            if (!hit.collider.CompareTag("Terrain")) {
+                Debug.DrawLine(sensorStartPos, hit.point);
+                avoiding = true;
+                avoidMultiplier += 1f;
+            }
+        }
+
+        //front left angle sensor
+        else if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)) {
+            if (!hit.collider.CompareTag("Terrain")) {
+                Debug.DrawLine(sensorStartPos, hit.point);
+                avoiding = true;
+                avoidMultiplier += 0.5f;
+            }
+        }
+
+        //front center sensor
+        if (avoidMultiplier == 0) {
+            if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)) {
+                if (!hit.collider.CompareTag("Terrain")) {
+                    Debug.DrawLine(sensorStartPos, hit.point);
+                    avoiding = true;
+                    if (hit.normal.x < 0) {
+                        avoidMultiplier = -1;
+                    } else {
+                        avoidMultiplier = 1;
+                    }
+                }
+            }
+        }
+
+        if (avoiding) {
+            targetSteerAngle = maxSteerAngle * avoidMultiplier;
+        }
+
+    }
+
+    private void ApplySteer() {
+        if (avoiding) return;
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currectNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        targetSteerAngle = newSteer;
+    }
+
+    private void Drive() {
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+
+        if (currentSpeed < maxSpeed && !isBraking) {
+            wheelFL.motorTorque = maxMotorTorque;
+            wheelFR.motorTorque = maxMotorTorque;
+        } else {
+            wheelFL.motorTorque = 0;
+            wheelFR.motorTorque = 0;
+        }
+    }
+
+    private void CheckWaypointDistance() {
+        if (Vector3.Distance(transform.position, nodes[currectNode].position) < 3f) {
+            if (currectNode == nodes.Count - 1) {
+                currectNode = 0;
+            } else {
+                currectNode++;
+            }
+        }
+    }
+
+    private void Braking() {
+        if (isBraking) {
+            carRenderer.material.mainTexture = textureBraking;
+            wheelRL.brakeTorque = maxBrakeTorque;
+            wheelRR.brakeTorque = maxBrakeTorque;
+        } else {
+            carRenderer.material.mainTexture = textureNormal;
+            wheelRL.brakeTorque = 0;
+            wheelRR.brakeTorque = 0;
+        }
+    }
+    private void LerpToSteerAngle() {
+        wheelFL.steerAngle = Mathf.Lerp(wheelFL.steerAngle, targetSteerAngle, Time.deltaTime * turnSpeed);
+        wheelFR.steerAngle = Mathf.Lerp(wheelFR.steerAngle, targetSteerAngle, Time.deltaTime * turnSpeed);
+    }
+}
